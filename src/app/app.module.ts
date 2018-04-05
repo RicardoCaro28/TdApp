d8a58f5a08f3fd4f6c0f322e0fbfb5094f423c36
Ricardo <luricarolo@ittepic.edu.mx>
2018-04-05 13:21:54 -0600

    requerimientos de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+///req de firebase
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
+import { FirebaseProvider } from '../providers/firebase/firebase';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +31,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
